d191a1e5241396d4708173d7b3f87dddeec0ffa9
Saxophilyman <zd_@mail.ru>
2025-12-08 03:17:48 +0400

    full CI/CD deploy 1.2

--- autopark-app/src/main/java/org/example/autopark/AutoparkApplication.java
+++ autopark-app/src/main/java/org/example/autopark/AutoparkApplication.java
@@ -3,12 +3,14 @@ package org.example.autopark;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 import java.util.TimeZone;
 
+@EntityScan("org.example.autopark")
 @SpringBootApplication
 @EnableCaching
 @EnableAspectJAutoProxy
