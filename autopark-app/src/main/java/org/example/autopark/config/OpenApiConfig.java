ecc013b2fbb7f8c447f199d89b07427c21e8fd1d
Saxophilyman <zd_@mail.ru>
2025-12-09 04:15:41 +0400

    full CI/CD deploy 3.1 (without kafka+notify)

--- autopark-app/src/main/java/org/example/autopark/config/OpenApiConfig.java
+++ autopark-app/src/main/java/org/example/autopark/config/OpenApiConfig.java
@@ -5,6 +5,7 @@ import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
@@ -14,9 +15,15 @@ import org.springframework.context.annotation.Configuration;
                 version = "9.0",
                 description = "REST API автопарка (менеджеры, предприятия, машины, водители)"
         ),
-        // Глобальное требование безопасности по умолчанию
         security = {
                 @SecurityRequirement(name = "bearerAuth")
+        },
+        servers = {
+                // относительный путь — Swagger будет использовать текущий хост и протокол
+                @Server(
+                        url = "/",
+                        description = "Тот же хост, что и Swagger (http/https определяется автоматически)"
+                )
         }
 )
 @SecurityScheme(
